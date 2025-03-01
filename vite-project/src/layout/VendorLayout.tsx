88d0580efb6595acc9dde7f790456d62972fc8ef
caonhathao <73047910+caonhathao@users.noreply.github.com>
2025-03-01 14:26:14 +0700

    update: vendor layout

--- vite-project/src/layout/VendorLayout.tsx
+++ vite-project/src/layout/VendorLayout.tsx
@@ -13,6 +13,9 @@ const VendorLayout = ({child}) => {
     const [activeTab, setActiveTab] = useState([false, false, false, false, false, false]);
     const [currentTab, setCurrentTab] = useState(-1);
     const [minimizeMenu, setMinimizeMenu] = useState(false);
+
+    const [posAnchor, setPosAnchor] = useState({top: 0, left: 0});
+
     const navRef = useRef<HTMLDivElement>(null);
     const constraintsRef = useRef<HTMLDivElement | null>(null)
 
@@ -56,10 +59,10 @@ const VendorLayout = ({child}) => {
                 fontSize: isClosed ? '16px' : '0',
             },
             transition: {
-                width: {duration: 0.5, delay: isClosed ? 0.2 : 0},
-                height: {duration: 0.5, delay: isClosed ? 0.2 : 0},
-                opacity: {duration: 0.5, delay: isClosed ? 0.6 : 0},
-                fontSize: {duration: 0.5, delay: isClosed ? 0.6 : 0},
+                width: {duration: 0.3, delay: isClosed ? 0.2 : 0},
+                height: {duration: 0.3, delay: isClosed ? 0.2 : 0},
+                opacity: {duration: 0.3, delay: isClosed ? 0.5 : 0},
+                fontSize: {duration: 0.3, delay: isClosed ? 0.5 : 0},
             },
             exit: {
                 width: isClosed ? 0 : 'max-content',
@@ -77,6 +80,54 @@ const VendorLayout = ({child}) => {
             animate(target, {scale: 1})
         }
     }))
+
+    useEffect(() => {
+        const anchor = document.querySelector("#anchorPoint");
+        if (anchor) {
+            const rect = anchor.getBoundingClientRect();
+            if (rect.top < 110 && rect.left < 15) {
+                setPosAnchor({
+                    top: rect.top + 45,
+                    left: rect.left + 5,
+                });
+            } else if (rect.top < 110 && rect.left > 300) {
+                setPosAnchor({
+                    top: rect.top + 45,
+                    left: rect.left - 5 - 50,
+                });
+            } else if (rect.top >= 110 && rect.top <= 400 && rect.left > 300) {
+                setPosAnchor({
+                    top: rect.top + 45,
+                    left: rect.left - 5 - 50,
+                });
+            } else if (rect.top > 440 && rect.left > 260) {
+                setPosAnchor({
+                    top: rect.top - 45 - 205,
+                    left: rect.left - 5 - 50,
+                });
+            } else if (rect.top > 440) {
+                setPosAnchor({
+                    top: rect.top - 45 - 205,
+                    left: rect.left - 5,
+                });
+            } else if (rect.left < 15) {
+                setPosAnchor({
+                    top: rect.top + 45,
+                    left: rect.left + 5,
+                });
+            } else if (rect.left > 300) {
+                setPosAnchor({
+                    top: rect.top - 45 - 205,
+                    left: rect.left - 5,
+                });
+            } else {
+                setPosAnchor({
+                    top: rect.top + 45,
+                    left: rect.left + 5,
+                });
+            }
+        }
+    }, [minimizeMenu]);
 
     return (
         <div className={'w-screen h-screen overflow-x-hidden'}>
@@ -104,59 +155,70 @@ const VendorLayout = ({child}) => {
                     <motion.div ref={constraintsRef}
                                 className={'w-85 max-h-screen fixed top-5 bottom-5 z-[9] pointer-events-none'}>
                         <motion.div ref={navRef}
-                                    className={`w-max h-max z-[10] absolute bg-gradient-to-b from-indigo-500 via-purple-500 to-pink-500 py-0.5 px-1 rounded-lg text-yellow-500 pointer-events-auto overflow-y-hidden`}
-                                    drag={'y'}
-                                    dragElastic={0.1}
+                                    className={`w-max h-max z-[10] flex flex-col absolute bg-gradient-to-b from-indigo-500 via-purple-500 to-pink-500 py-0.5 px-1 rounded-lg text-yellow-300 pointer-events-auto overflow-y-hidden shadow-lg shadow-gray-600`}
+                                    drag
+                                    dragElastic={0.01}
                                     dragMomentum={true}
                                     dragConstraints={constraintsRef}
+                                    id={'anchorPoint'}
                         >
-                            <div className={'flex flex-col items-center justify-center order-1 px-1 my-2 '}
-                                 onClick={closeMenu}>
+                            <div
+                                className={'w-fit h-fit flex flex-col relative items-center justify-center order-1 px-1 my-2 '}
+                                onClick={closeMenu}>
                                 {!minimizeMenu ?
                                     <Bs.BsCaretRightFill className={'border rounded-4xl'} size={20} color={'white'}/>
                                     :
                                     <Bs.BsCaretLeftFill className={'border rounded-4xl'} size={20} color={'white'}/>}
                             </div>
-                            <MotionConfig>
-                                <AnimatePresence initial={false}>
-                                    {minimizeMenu ? (
-                                        <motion.ul id={'navBtn'}
-                                                   {...getMotionConfig(minimizeMenu)}
-                                                   className={`w-full h-full`}>
-                                            <li
-                                                className={`text-center p-2 ${activeTab[0] ? `bg-gradient-to-l from-red-400 to-teal-400 text-white font-bold` : `bg-transparent`}`}
-                                                onClick={() => activeCurrTab(0)}>
-                                                <Link to={'/'}>Tổng quan</Link></li>
-                                            <li className={`text-center py-2 ${activeTab[1] ? `bg-gradient-to-l from-red-400 to-teal-400 text-white font-bold` : `bg-transparent`}`}
-                                                onClick={() => activeCurrTab(1)}>
-                                                <Link
-                                                    to={'/manage'}>Quản lí</Link>
-                                            </li>
-                                            <li className={`text-center p-2 ${activeTab[2] ? `bg-gradient-to-l from-red-400 to-teal-400 text-white font-bold` : `bg-transparent`}`}
-                                                onClick={() => activeCurrTab(2)}>
-                                                <Link
-                                                    to={'/orders'}>Đơn hàng</Link>
-                                            </li>
-                                            <li className={`text-center p-2 ${activeTab[3] ? `bg-gradient-to-l from-red-400 to-teal-400 text-white font-bold` : `bg-transparent`}`}
-                                                onClick={() => activeCurrTab(3)}>
-                                                <Link
-                                                    to={'/support'}>Hỗ trợ</Link>
-                                            </li>
-                                            <li className={`text-center p-2 ${activeTab[4] ? `bg-gradient-to-l from-red-400 to-teal-400 text-white font-bold` : `bg-transparent`}`}
-                                                onClick={() => activeCurrTab(4)}>
-                                                <Link
-                                                    to={'/shop-info'}>Cá nhân</Link>
-                                            </li>
-                                            <li className={`text-center p-2 ${activeTab[5] ? `bg-gradient-to-l from-red-400 to-teal-400 text-white font-bold` : `bg-transparent`}`}
-                                                onClick={() => signOut()}>
-                                                Đăng xuất
-                                            </li>
-                                        </motion.ul>
-                                    ) : null}
-                                </AnimatePresence>
-                            </MotionConfig>
                         </motion.div>
                     </motion.div>
+                    <div className={'w-fit h-fit z-[10]'}
+                         style={{
+                             position: 'fixed',
+                             top: posAnchor.top,
+                             left: posAnchor.left,
+                         }}>
+                        <MotionConfig>
+                            <AnimatePresence initial={false}>
+                                {minimizeMenu ? (
+                                    <motion.ul id={'navBtn'}
+                                               {...getMotionConfig(minimizeMenu)}
+                                               className={`w-full h-full bg-gradient-to-b from-indigo-500 via-purple-500 to-pink-500 py-0.5 px-1 rounded-lg text-yellow-300 pointer-events-auto overflow-y-hidden shadow-lg shadow-gray-600`}
+                                    >
+                                        <li
+                                            className={`text-center p-2 ${activeTab[0] ? `bg-gradient-to-l from-red-400 to-teal-400 text-white font-bold` : `bg-transparent`} rounded-md`}
+                                            onClick={() => activeCurrTab(0)}>
+                                            <Link to={'/'}>Tổng quan</Link></li>
+                                        <li className={`text-center py-2 ${activeTab[1] ? `bg-gradient-to-l from-red-400 to-teal-400 text-white font-bold` : `bg-transparent`} rounded-md`}
+                                            onClick={() => activeCurrTab(1)}>
+                                            <Link
+                                                to={'/manage'}>Quản lí</Link>
+                                        </li>
+                                        <li className={`text-center p-2 ${activeTab[2] ? `bg-gradient-to-l from-red-400 to-teal-400 text-white font-bold` : `bg-transparent`} rounded-md`}
+                                            onClick={() => activeCurrTab(2)}>
+                                            <Link
+                                                to={'/orders'}>Đơn hàng</Link>
+                                        </li>
+                                        <li className={`text-center p-2 ${activeTab[3] ? `bg-gradient-to-l from-red-400 to-teal-400 text-white font-bold` : `bg-transparent`} rounded-md`}
+                                            onClick={() => activeCurrTab(3)}>
+                                            <Link
+                                                to={'/support'}>Hỗ trợ</Link>
+                                        </li>
+                                        <li className={`text-center p-2 ${activeTab[4] ? `bg-gradient-to-l from-red-400 to-teal-400 text-white font-bold` : `bg-transparent`} rounded-md`}
+                                            onClick={() => activeCurrTab(4)}>
+                                            <Link
+                                                to={'/shop-info'}>Cá nhân</Link>
+                                        </li>
+                                        <li className={`text-center p-2 ${activeTab[5] ? `bg-gradient-to-l from-red-400 to-teal-400 text-white font-bold` : `bg-transparent`} rounded-md`}
+                                            onClick={() => signOut()}>
+                                            Đăng xuất
+                                        </li>
+                                    </motion.ul>
+                                ) : null}
+                            </AnimatePresence>
+                        </MotionConfig>
+                    </div>
+
                     <div className={'w-full h-full flex flex-row items-center justify-center mt-2 px-2'}>
                         {child}
                     </div>

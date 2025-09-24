778a52fcfa7835bfa359e32982e5b58eb1c26d0a
caonhathao <caonhathao2372004@gmail.com>
2025-09-25 03:30:27 +0700

    Update

--- vite-project/src/pages/admin/dashboard/AdminDashboard.tsx
+++ vite-project/src/pages/admin/dashboard/AdminDashboard.tsx
@@ -1,4 +1,14 @@
+import SectionCards from "@/components/ui-custom/admin/dashboard/section-cards";
+
+
 const AdminDashboard = () => {
-    return <div className={'flex-1'}>Admin Dashboard</div>;
-}
+  return (
+    <div className={"flex-1 p-2 w-full h-full"}>
+      <div className="w-full h-full flex flex-col justify-start items-center gap-4">
+        {/* Showing KPI/CARDS */}
+        <SectionCards />
+      </div>
+    </div>
+  );
+};
 export default AdminDashboard;
